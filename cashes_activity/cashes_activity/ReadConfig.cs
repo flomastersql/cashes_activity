4adeee8c2d616c87418962fe301b2585ee7f3f5f
Сорокин Андрей Алексеевич <a_sorokin@priminvestor.lan>
2021-08-05 18:29:12 +1000

    ошибка с нулями и исклюатор из текстовика

--- cashes_activity/cashes_activity/ReadConfig.cs
+++ cashes_activity/cashes_activity/ReadConfig.cs
@@ -8,5 +8,10 @@ namespace cashes_activity
         {
             return File.ReadAllLines(@"C:\scripts\cashes_activity\keeper_rests2.ini");
         }
+
+        public static string ReadCashesRetroExclude()
+        {
+            return File.ReadAllText(@"C:\scripts\cashes_activity\cashes_retro_exclude.txt");
+        }
     }
 }
